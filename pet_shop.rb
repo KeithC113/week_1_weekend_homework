71289eb99609707b40f068365c5b64cbd889b847
KeithC113 <campbell_ka@yahoo.co.uk>
2020-05-10 10:34:59 +0100

    changes upto test function No5

--- pet_shop.rb
+++ pet_shop.rb
@@ -6,9 +6,11 @@ def total_cash(cash)
   return cash[:admin][:total_cash]
 end
 
-def add_or_remove_cash__add
-  def add_or_remove_cash(pet_shop,cash_to_add)
-    new_cash_total=pet_shop[:admin][:total_cash]+10
-    return new_cash_total
-  end
+def add_or_remove_cash(pet_shop, cash_to_add_or_remove)
+    pet_shop[:admin][:total_cash] =  pet_shop[:admin][:total_cash] + cash_to_add_or_remove
+    return
+end
+
+def pets_sold(shop)
+  return shop[:admin][:pets_sold]
 end
