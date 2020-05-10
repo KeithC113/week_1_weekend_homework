4c5aca929dc1ff60cf59660cba832ca8be405b3e
KeithC113 <campbell_ka@yahoo.co.uk>
2020-05-10 16:48:50 +0100

    almost thee - upto Q14

--- pet_shop.rb
+++ pet_shop.rb
@@ -37,9 +37,49 @@ def pets_by_breed(pet_shop, breed)
 end
 
 def find_pet_by_name(pet_shop, pet_name)
+# for all pets in pet shop
   for pet in pet_shop[:pets]
+# if pet name equals pet_name then deliver this
     if pet[:name] == pet_name
-      return pet_name
+      return pet
+    # elsif return
+
     end
   end
-end 
+end
+
+# def remove_pet_by_name(pet_shop, pet_name)
+# # search all pets within the shop
+#   for pet in pet_shop[:pets]
+# # if the pets name is equal to pet_name
+#     if pet[:name] == pet_name
+# #  then remove this pet from list of pets in pet_shop
+#         pet_shop[:pets].delete(pets[:name])
+#     end
+#   end
+#   return pet_shop
+# end
+
+def add_pet_to_stock(pet_shop,new_pet)
+  pet_shop[:pets].push([new_pet])
+  return pet_shop
+end
+
+def customer_cash(customers_cash)
+  return customers_cash[:cash]
+end
+
+def remove_customer_cash(customer,cash_value)
+  customer[:cash] = customer[:cash] - cash_value
+  return
+end
+
+def customer_pet_count(customer)
+  number_of_pets = customer[:pets].count.to_i
+  return
+end
+
+def add_pet_to_customer(customer_name,new_pet)
+  customer_name[:pets].push([new_pet])
+  return
+end
